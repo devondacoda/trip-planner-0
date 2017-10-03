aba3b3ff5b51e55a4e5416110d39980d2f2396db
Devon JohnBaptiste <devondevon@outlook.com>
2017-10-03 15:39:46 -0400

    aligned sidebar and map

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 const markers = require('./marker');
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3Bvb25zMTIzIiwiYSI6ImNqOGJydmF2bDAxNXIycW8wd2MxZWV0YXMifQ.e2tHK-kLXKdoXZThDMOMEw';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack coordinates
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
 });
 
 const marker = document.createElement('div');
-marker.style.width = "32px";
-marker.style.height = "39px";
-marker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+marker.style.width = '32px';
+marker.style.height = '39px';
+marker.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+
 new mapboxgl.Marker(marker).setLngLat([-74.009, 40.705]).addTo(map);
 
 //mapbox login: spoons123 , FullstackACHU
--- src/marker.js
+++ src/marker.js
@@ -3,11 +3,11 @@ function makeMarker(thingToDo, coordinates) {
     activity: 'http://i.imgur.com/WbMOfMl.png',
     hotel: 'http://i.imgur.com/D9574Cu.png',
     restaurant: 'http://i.imgur.com/cqR6pUI.png'
-  }
+  };
 
   const marker = document.createElement('div', {id: thingToDo});
-  marker.style.width = "32px";
-  marker.style.height = "39px";
+  marker.style.width = '32px';
+  marker.style.height = '39px';
   marker.style.backgroundImage = options[marker.id];
 
   new mapboxgl.Marker(marker).setLngLat(coordinates);
